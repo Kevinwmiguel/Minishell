37903c51cb9ecdc89614a4d3d08565d2e9e4faf3
jmehmy <jmehmy@ubuntu.Home>
2025-06-11 22:46:30 +0100

    create parsing file

---
+++ parsing/parsing.c
@@ -0,0 +1,12 @@
+#include "minishell.h"
+
+int	parse(t_shell *data, char *str)
+{
+    char *str_parse;
+    if(ft_strlen(str) == 0)
+        return(0);
+    if(str_parse == NULL)
+        return(0);
+    free(str_parse);
+    return(0);
+}
