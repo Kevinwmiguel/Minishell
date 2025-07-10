00d4cc4c84c08f93d790d4ef570c74cfc44a28cb
Jass <jaskaran.prince1@gmail.com>
2025-07-10 23:01:21 +0100

    Update build_cd.c

--- src/builtins/build_cd.c
+++ src/builtins/build_cd.c
@@ -51,7 +51,10 @@ void	build_cd(t_shell *shell)
 	else
 		path = ft_strdup(shell->cmd->args[1]);
 	if (chdir(path) != 0)
+	{
 		perror("minishell: cd");
+		shell->exit_code = 1;
+	}
 	else
 		update_pwd(shell);
 	free(path);
