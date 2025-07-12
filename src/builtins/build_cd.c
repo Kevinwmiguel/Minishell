6abe672e406077256dea372acb05e2cf9c7ee916
jmehmy <jmehmy@ubuntu.Home>
2025-07-12 23:55:56 +0100

    when arguement are many

--- src/builtins/build_cd.c
+++ src/builtins/build_cd.c
@@ -37,6 +37,12 @@ void	build_cd(t_shell *shell)
 	char	*home;
 
 	path = NULL;
+	if (shell->cmd->args[2])
+	{
+		ft_putstr_fd(" too many arguments\n", 2);
+		shell->exit_code = 1;
+		return ;
+	}
 	if (!shell->cmd->args[1] || shell->cmd->args[1][0] == '\0')
 	{
 		home = get_home(shell);
@@ -52,7 +58,7 @@ void	build_cd(t_shell *shell)
 		path = ft_strdup(shell->cmd->args[1]);
 	if (chdir(path) != 0)
 	{
-		perror("minishell: cd");
+		perror("cd");
 		shell->exit_code = 1;
 	}
 	else
